51d2d990379f40095d4aa1ede89898bae1e86210
Avrumi Friedman <alex+6@lakewoodprogramming.com>
2018-05-15 21:33:19 -0400

    Initial commit

--- ConsoleApp76/Program.cs
+++ ConsoleApp76/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Linq;
 
 namespace ConsoleApp76
 {
@@ -10,6 +11,48 @@ namespace ConsoleApp76
     {
         static void Main(string[] args)
         {
+            using (var context = new PeopleCarsDataContext())
+            {
+                context.Log = Console.Out;
+
+                //var loadOptions = new DataLoadOptions();
+                //loadOptions.LoadWith<Person>(p => p.Cars);
+                //context.LoadOptions = loadOptions;
+
+                //foreach (Person person in context.Persons)
+                //{
+                //    Console.WriteLine($"{person.FirstName} {person.LastName}");
+                //    foreach (Car car in person.Cars)
+                //    {
+                //        Console.WriteLine($"\t{car.Make} {car.Model}");
+                //    }
+                //}
+
+                //Car car = context.Cars.FirstOrDefault(c => c.Id == 4);
+                //Console.WriteLine(car.Person.FirstName + " " + car.Person.LastName);
+
+                //Person person = context.Persons.FirstOrDefault(p => p.Id == 1028);
+                //Car car = new Car
+                //{
+                //    Make = "Porsche",
+                //    Model = "911",
+                //    Year = 2017
+                //};
+                //person.Cars.Add(car);
+                //context.SubmitChanges();
+                //Console.WriteLine(car.PersonId);
+                //Car car = new Car
+                //{
+                //    Make = "Nissan",
+                //    Model = "GTR",
+                //    Year = 2018,
+                //    PersonId = 1029
+                //};
+                //context.Cars.InsertOnSubmit(car);
+                //context.SubmitChanges();
+            }
+
+            Console.ReadKey(true);
         }
     }
 }
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PeopleCars.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var repo = new PeopleCarsRepository(Properties.Settings.Default.ConStr);
+            return View(repo.GetPeople());
+        }
+
+        public ActionResult AddCar(int personId)
+        {
+            var repo = new PeopleCarsRepository(Properties.Settings.Default.ConStr);
+            var person = repo.GetById(personId);
+            if (person == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult AddCar(Car car)
+        {
+            var repo = new PeopleCarsRepository(Properties.Settings.Default.ConStr);
+            repo.AddCar(car);
+            return RedirectToAction("Index");
+        }
+    }
+}
